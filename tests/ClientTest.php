30acb774f4d4477ad5bc43490872849308e4d2ea
AlexisPPLIN <contact@alexispoupelin.com>
2026-02-05 14:35:50 +0100

    Fixed coverage + added cache in ci

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Test\AlexisPPLIN\SendcloudV3;
 
+use AlexisPPLIN\SendcloudV3\Client;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
+#[CoversClass(Client::class)]
 class ClientTest extends TestCase
 {
     public function testEmpty(): void
